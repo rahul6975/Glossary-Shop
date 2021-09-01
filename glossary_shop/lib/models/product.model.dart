e1122cd6465653d188592c67844faae81254e697
rahul6975 <rahulya7569@gmail.com>
2021-09-01 09:08:53 +0530

    models created

---
+++ glossary_shop/lib/models/product.model.dart
@@ -0,0 +1,66 @@
+import 'package:get/get.dart';
+
+class ProductModel {
+  ProductModel({
+    required int id,
+    required String name,
+    required String category,
+    required double price,
+    required String image,
+    required String description,
+  }) {
+    this.id = id;
+    this.name = name;
+    this.category = category;
+    this.price = price;
+    this.image = image;
+    this.description = description;
+    this.like = false;
+  }
+
+  RxInt _id = RxInt();
+
+  set id(int value) => _id.value = value;
+
+  int get id => _id.value;
+
+  RxString _name = RxString();
+
+  set name(String value) => _name.value = value;
+
+  String get name => _name.value;
+
+  RxString _category = RxString();
+
+  set category(String value) => _category.value = value;
+
+  String get category => _category.value;
+
+  RxDouble _price = RxDouble();
+
+  set price(double value) => _price.value = value;
+
+  double get price => _price.value;
+
+  RxString _image = RxString();
+
+  set image(String value) => _image.value = value;
+
+  String get image => _image.value;
+
+  RxString _description = RxString();
+
+  set description(String value) => _description.value = value;
+
+  String get description => _description.value;
+
+  RxBool _like = RxBool();
+
+  set like(bool value) => _like.value = value;
+
+  bool get like => _like.value;
+
+  String get dollar {
+    return "U\$ " + this.price.toStringAsFixed(2);
+  }
+}
