bd1772dc592b487ef7bcea8701fb6935ce2a9a28
rahul6975 <rahulya7569@gmail.com>
2021-09-01 09:12:05 +0530

    models created

--- glossary_shop/lib/models/cart_item.model.dart
+++ glossary_shop/lib/models/cart_item.model.dart
@@ -3,18 +3,23 @@ import './product.model.dart';
 
 class CartItemModel {
   CartItemModel({
-    ProductModel product,
-    int quantity,
+    required ProductModel product,
+    required int quantity,
   }) {
     this.product = product;
     this.quantity = quantity;
   }
+
   Rx<ProductModel> _product = Rx<ProductModel>();
+
   set product(ProductModel value) => _product.value = value;
+
   ProductModel get product => _product.value;
 
   RxInt _quantity = RxInt();
+
   set quantity(int value) => _quantity.value = value;
+
   int get quantity => _quantity.value;
 
   incrementQuantity() {
